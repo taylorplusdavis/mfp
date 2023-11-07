57013fdf24c2963eb2c625eba3ec3bdd1995f688
Taylor Davis <38449666+taylorplusdavis@users.noreply.github.com>
2023-11-07 10:33:33 -0600

    styling bug fix

--- container/src/App.js
+++ container/src/App.js
@@ -3,14 +3,21 @@ import { mount } from "marketing/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 const App = () => {
   console.log(mount);
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
