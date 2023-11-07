9372274f60644885fe44744ebe425bab7a57153e
Taylor Davis <38449666+taylorplusdavis@users.noreply.github.com>
2023-11-07 14:16:30 -0600

    add auth

--- auth/config/webpack.prod.js
+++ auth/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "http://localhost:8082/",
+    publicPath: "/auth/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
